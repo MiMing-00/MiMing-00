45942db90c07162e515df6722d14a2a2c40854ec
MiMing-00 <160488193+MiMing-00@users.noreply.github.com>
2024-05-22 09:48:07 +0900

    [level 0] Title: 특정 문자 제거하기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120826. 특정 문자 제거하기/특정 문자 제거하기.js
+++ 프로그래머스/0/120826. 특정 문자 제거하기/특정 문자 제거하기.js
@@ -1,11 +1,3 @@
 function solution(my_string, letter) {
-    let answer =[];
-    let nextMyStr = my_string.split('');
-    
-    for (let i=0; i<nextMyStr.length; i++) {
-        if (nextMyStr[i] !== letter) {
-            answer.push(nextMyStr[i])
-        }
-    }
-    return answer.join("");
+    return my_string.replaceAll(letter, '')
 }
